8cf41f0e4cc4e1e90a7849801ecb6e9deb81ed07
tangxin <tangxin@tangxin-mbp.local>
2019-03-13 14:25:34 +0800

    add docker-install.sh

---
+++ docker-compose/docker-install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+
+function centos()
+{
+    # step 1: 安装必要的一些系统工具
+    sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+    # Step 2: 添加软件源信息
+    sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+    # Step 3: 更新并安装 Docker-CE
+    sudo yum makecache fast
+    sudo yum -y install docker-ce
+    # Step 4: 开启Docker服务
+    sudo service docker start
+
+}
+
+function ubuntu()
+{
+    # step 1: 安装必要的一些系统工具
+    sudo apt-get update
+    sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+    # step 2: 安装GPG证书
+    curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+    # Step 3: 写入软件源信息
+    sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+    # Step 4: 更新并安装 Docker-CE
+    sudo apt-get -y update
+    sudo apt-get -y install docker-ce
+
+}
+
+case $1 in )
+centos|ubuntu) $1 ;;
+esac
