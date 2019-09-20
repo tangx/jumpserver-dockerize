1f319e8e7932294302449b2ae045cc7501a7ea5b
tangxin <tangxin@tangxin-mbp.local>
2019-09-20 16:47:26 +0800

    UDPATE GUACAMOLE

--- dockerfiles/guacamole/entrypoint.sh
+++ dockerfiles/guacamole/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #
 
-/etc/init.d/guacd start
-/config/tomcat8/bin/catalina.sh run
+# /etc/init.d/guacd start
+# /config/tomcat8/bin/catalina.sh run
 
+localedef -c -f UTF-8 -i zh_CN zh_CN.UTF-8
+export LC_ALL=zh_CN.UTF-8
+
+guacd &
+# dockerize -wait ${JUMPSERVER_SERVER} /config/tomcat8/bin/catalina.sh run 
+/config/tomcat8/bin/catalina.sh run 
